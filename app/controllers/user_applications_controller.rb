7586a0f887f9d99455c3b09dae8eb6fb121b6853
Travis Vesbach <tav1990@hotmail.com>
2023-12-02 20:15:06 -0600

    Add functionality so users can remove applications from their dashboard

--- app/controllers/user_applications_controller.rb
+++ app/controllers/user_applications_controller.rb
@@ -6,4 +6,11 @@ class UserApplicationsController < ApplicationController
     @user_applications = user.user_applications
     @available_applications = Application.where.not(id: UserApplication.where(user_id: user.id).pluck(:application_id))
   end
+
+  def destroy
+    user_application = UserApplication.find(params[:id])
+    user_application.destroy
+
+    redirect_to user_applications_path
+  end
 end
